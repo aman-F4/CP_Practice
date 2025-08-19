2e2e655cc791033e112acc04128e2b8ff02d6e86
Aman Verma <857449av@gmail.com>
2025-08-19 14:27:03 +0530

    Practice problem

---
+++ 22_String/01_reverse_of_string.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+void printArray(int arr[],int n){
+    for( int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+    void swapArray(int arr[],int size){
+        for( int i=0;i<size;i+=2){
+            if(i+1<size){
+                swap(arr[i],arr[i+1]);
+            }
+        }
+    }
+
+int main(){
+    int even[8]={1,2,3,4,5,6,7,8};
+    int odd[5]={1,3,4,6,8};
+    swapArray(even,8);
+    printArray(even,8);
+    cout<<endl;
+    swapArray(odd,5);
+    printArray(odd,5);
+
+    return 0;
+}
